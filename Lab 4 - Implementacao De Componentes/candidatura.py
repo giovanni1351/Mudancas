9e1e8ba0df85803c4a0cb056bc7e4eb85391886d
Henrique Finatti <finatti.henrique@gmail.com>
2026-03-13 17:14:40 -0300

    Adicionando componentes

---
+++ Lab 4 - Implementacao De Componentes/candidatura.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+class CandidaturaService(ABC):
+
+    @abstractmethod
+    def validarInformaçõesCandidaturas(self, candidatura):
+        raise NotImplementedError("Metodo para validar informações de candidatura não implementado.")
+
+    @abstractmethod
+    def registrarCandidatura(self, candidaturaValidada):
+        raise NotImplementedError("Metodo para registrar candidatura não implementado.")
+
+    @abstractmethod
+    def mostrarListaCandidatos(self, demanda):
+        raise NotImplementedError("Metodo para mostrar lista de candidatos.")
+
+class Candidatura(CandidaturaService):
+    def validarInformaçõesCandidaturas(self, candidatura):
+        pass
+
+    def registrarCandidatura(self, candidaturaValidada):
+        pass
+
+    def mostrarListaCandidatos(self, demanda):
+        pass
---
+++ Lab 4 - Implementacao De Componentes/demanda.py
@@ -0,0 +1,39 @@
+
+from abc import ABC, abstractmethod
+
+class DemandaService(ABC):
+    @abstractmethod
+    def validarInformações(self, demanda):
+        raise NotImplementedError("Metodo para validar informações de demanda não implementado.")
+
+    @abstractmethod
+    def registrarDemanda(self, demandaValidada):
+        raise NotImplementedError("Metodo para registrar demanda não implementado.")
+
+    @abstractmethod
+    def buscarTodasDemandas(self):
+        raise NotImplementedError("Metodo para buscar todas as demandas não implementado.")
+
+    @abstractmethod
+    def mostrarDetalheDemanda(self, demandaId):
+        raise NotImplementedError("Metodo para mostrar detalhes da demanda não implementado.")
+
+    @abstractmethod
+    def associaCaminhoneiroDemanda(self, caminhoneiro,demanda):
+        raise NotImplementedError("Metodo para associar caminhoneiro a demanda não implementado.")
+
+class Demanda(DemandaService):
+    def validarInformações(self, demanda):
+        pass
+
+    def registrarDemanda(self, demandaValidada):
+        pass
+
+    def buscarTodasDemandas(self):
+        pass
+
+    def mostrarDetalheDemanda(self, demandaId):
+        pass
+
+    def associaCaminhoneiroDemanda(self, caminhoneiro,demanda):
+        pass
